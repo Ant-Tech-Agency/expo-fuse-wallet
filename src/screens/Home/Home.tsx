22a5c6e2992562f11493acb682f77b9e8ca4e957
linh <tuan-linh.tran@ant-tech.eu>
2019-08-27 15:52:28 +0700

    Update UI (add scroll view)

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,4 +1,4 @@
-import {Image, KeyboardAvoidingView, SafeAreaView, Text, TouchableOpacity, View} from 'react-native'
+import {Image, KeyboardAvoidingView, SafeAreaView, ScrollView, Text, TouchableOpacity, View} from 'react-native'
 import React, {useEffect, useState} from 'react'
 import {WalletEffect, walletEffect} from '@/effects/wallet.effect'
 import {useNavigation} from 'react-navigation-hooks'
@@ -30,77 +30,79 @@ export const Home: React.FC = () => {
     <SafeAreaView style={{
       flex: 1
     }}>
-      <KeyboardAvoidingView style={styles.container}>
-        <Image style={Styles.logo} source={require("../../../assets/logo.png")} />
-        <Text style={styles.titleScreen}>
-          Wallet Info
-        </Text>
-        <View style={{flex: 1}}>
-          <Text style={styles.textCategory}>Fusion Balance: </Text>
-          <View style={styles.wrapBalance}>
-            <Text style={styles.textBalance}>
-              {
-                loading ?
-                  'Loading...' :
-                  `${balance}`
-              } <Text style={styles.textCategory}>FSN</Text>
+      <ScrollView>
+        <KeyboardAvoidingView style={styles.container}>
+          <Image style={Styles.logo} source={require('../../../assets/logo.png')}/>
+          <Text style={styles.titleScreen}>
+            Wallet Info
+          </Text>
+          <View style={{flex: 1}}>
+            <Text style={styles.textCategory}>Fusion Balance: </Text>
+            <View style={styles.wrapBalance}>
+              <Text style={styles.textBalance}>
+                {
+                  loading ?
+                    'Loading...' :
+                    `${balance}`
+                } <Text style={styles.textCategory}>FSN</Text>
+              </Text>
+            </View>
+            <Text style={styles.textCategory}>Public Address:</Text>
+            <Text style={styles.textPublicAddress}>{walletStore.wallet.address}</Text>
+          </View>
+          <View style={styles.wrapInput}>
+            <Text style={styles.titleFeature}>
+              Asset Creation
             </Text>
+            <View style={{flex: 1, justifyContent: 'center'}}>
+              <AInput name={'Asset Name:'}/>
+              <AInput name={'Supply:'}/>
+              <TouchableOpacity
+                style={styles.button}
+              >
+                <Text style={styles.titleButton}>
+                  Create Asset
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Text style={styles.textCategory}>Public Address:</Text>
-          <Text style={styles.textPublicAddress}>{walletStore.wallet.address}</Text>
-        </View>
-        <View style={styles.wrapInput}>
-          <Text style={styles.titleFeature}>
-            Asset Creation
-          </Text>
-          <View style={{flex: 1, justifyContent: 'center'}}>
-            <AInput name={'Asset Name:'}/>
-            <AInput name={'Supply:'}/>
-            <TouchableOpacity
-              style={styles.button}
-            >
-              <Text style={styles.titleButton}>
-                Create Asset
-              </Text>
-            </TouchableOpacity>
+          <View style={styles.wrapInput}>
+            <Text style={styles.titleFeature}>
+              Send Asset
+            </Text>
+            <View style={{flex: 1, justifyContent: 'center'}}>
+              <AInput name={'To:'}/>
+              <AInput name={'Quantity:'}/>
+              <TouchableOpacity
+                style={styles.button}
+              >
+                <Text style={styles.titleButton}>
+                  Send Asset
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-        <View style={styles.wrapInput}>
-          <Text style={styles.titleFeature}>
-            Send Asset
-          </Text>
-          <View style={{flex: 1, justifyContent: 'center'}}>
-            <AInput name={'To:'}/>
-            <AInput name={'Quantity:'}/>
-            <TouchableOpacity
-              style={styles.button}
-            >
-              <Text style={styles.titleButton}>
-                Send Asset
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        {/*<TouchableOpacity*/}
-        {/*	style={{*/}
-        {/*		backgroundColor: '#81AFE0',*/}
-        {/*		padding: 10,*/}
-        {/*		margin: 10,*/}
-        {/*		width: '100%'*/}
-        {/*	}}*/}
-        {/*	onPress={async () => {*/}
-        {/*		await walletStore.deletePrivateKey()*/}
-        {/*		navigate('AccessWallet')*/}
-        {/*	}}*/}
-        {/*>*/}
-        {/*	<Text style={{*/}
-        {/*		color: 'white',*/}
-        {/*		fontSize: 16*/}
-        {/*	}}>*/}
-        {/*		Log out*/}
-        {/*	</Text>*/}
-        {/*</TouchableOpacity>*/}
-      </KeyboardAvoidingView>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#81AFE0',
+              padding: 10,
+              margin: 10,
+              width: '100%'
+            }}
+            onPress={async () => {
+              await walletStore.deletePrivateKey()
+              navigate('AccessWallet')
+            }}
+          >
+            <Text style={{
+              color: 'white',
+              fontSize: 16
+            }}>
+              Log out
+            </Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
     </SafeAreaView>
   )
 }
